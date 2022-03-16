c3c368b618eaf1471c17241b416f2853acc302ad
alisherbegmatov <alisher.begmatov@students.dominican.edu>
2022-03-16 16:00:24 -0700

    Created Performance.js
    Created Performance.js component

---
+++ src/components/Performance/Performance.js
@@ -0,0 +1,11 @@
+function Performance () {
+  return (
+    <div className="min-h-screen relative before:absolute before:top-0 before:left-0 before:opacity-50 before:bg-6 before:bg-no-repeat before:bg-cover before:content-[' '] before:w-full before:h-full flex items-center justify-center gap-8">
+      <div className='flex flex-row px-60 justify-around gap-8 mb-36 z-40'>
+        <h1 className='w-[23vw] text-white font-serif font-semibold text-4xl -mt-20'>Pulse-racing acceleration made to measure</h1>
+        <span className='w-[30vw] text-[#8c8c99] text-xl'>The new BMW M8 Gran Coupe First Edition 1 of 8 combines the character of a sports car with the spirit of the BMW luxury class, creating a new form of aesthetics.</span>
+      </div>
+    </div>
+  )
+}
+export default Performance
