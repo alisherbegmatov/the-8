6269fe124da1a50bfd2f5cb62a1419be31d3c590
alisherbegmatov <alisher.begmatov@students.dominican.edu>
2022-03-17 14:30:01 -0700

    Updated App.js
    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header'
+import BurgerMenu from './components/burgerMenu/BurgerMenu'
+import Main from './components/main/Main'
+import Gallery from './components/gallery/Gallery'
+import Design from './components/Design/Design'
+import NewsLetter from './components/NewsLetter/NewsLetter'
+import Creator from './components/Creator/Creator'
+import Shape from './components/Shape/Shape'
+import Performance from './components/Performance/Performance'
+import Information from './components/Information/Information'
+import Footer from './components/footer/Footer'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App relative'>
+      <Header />
+      <BurgerMenu />
+      <Main />
+      <Creator />
+      <Design />
+      <NewsLetter />
+      <Gallery />
+      <Performance />
+      <Shape />
+      <Information />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
